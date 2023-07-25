52f1be170804970def22a38611f185a98ac90a9b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-07-25 17:57:33 +0530

    Add files via upload

---
+++ tests/test_cases.cpp
@@ -0,0 +1,46 @@
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <iostream>
+#include <gtest/gtest.h>
+#include "../fileOperations.cpp"
+#include "./utils/testUtils.cpp"
+
+using namespace curlpp::options;
+
+TEST(CppFileHandlingProjectTest, FilePresentTrue) {
+    const int filePresent = isFilePresent("sample.txt");
+    TestUtils t;
+    t.yakshaAssert("FilePresentTrue", filePresent, "boundary");    
+    ASSERT_EQ(1, filePresent);
+}
+
+TEST(CppFileHandlingProjectTest, CreateFileAndWriteDataTrue) {
+    const int dataWritten = createFileAndWriteData("test1.txt", "some sample data");
+    const int fileCreated = dataWritten ? readData("test1.txt").length() ? 1 : 0 : 0;
+    TestUtils t;
+    t.yakshaAssert("CreateFileAndWriteDataTrue", dataWritten, "functional");    
+    ASSERT_EQ(1, dataWritten);
+}
+
+TEST(CppFileHandlingProjectTest, SearchTextTrue) {
+    const int textPresent = searchText("test1.txt", "string search") ? 1 : 0;
+    TestUtils t;
+    t.yakshaAssert("SearchTextTrue", textPresent, "exception");    
+    ASSERT_EQ(1, textPresent);
+}
+
+TEST(CppFileHandlingProjectTest, ReadDataTrue) {
+    const int readedData = readData("test1.txt").length() ? 1 : 0;
+    TestUtils t;
+    t.yakshaAssert("ReadDataTrue", readedData, "boundary");    
+    ASSERT_EQ(1, readedData);
+}
+
+TEST(CppFileHandlingProjectTest, OpenFileAndAppendDataTrue) {
+    const string data = "\nextra data";
+    const int appendData = openFileAndAppendData("test1.txt", data);
+    TestUtils t;
+    t.yakshaAssert("OpenFileAndAppendDataTrue", appendData, "functional");    
+    ASSERT_EQ(1, appendData);
+}
---
+++ tests/utils/testUtils.cpp
@@ -0,0 +1,90 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <gtest/gtest.h>
+#include <nlohmann/json.hpp>
+#include "testResults.cpp"
+#include "testCaseResult.cpp"
+
+using json = nlohmann::json;
+using namespace curlpp::options;
+using namespace std;
+
+class TestUtils {
+
+	public:
+        string testResult;
+        const string URL = "https://yaksha-stage-sbfn.azurewebsites.net/api/TestCaseResultsEnqueue?code=AjU0mofZlYs9oYbZnJpVwJWRY1dRKkDyS3QDY8aJAvrcjJvgBAXVDg==";
+        const string GUID = "f1ab8032-3cdf-4faf-a4aa-008946815a38";
+        string customData;
+	
+		string readCustomFileData(string filePath) {
+			ifstream inputFile(filePath);
+			string line;
+			string name;
+			try {
+				while (inputFile >> name) {
+					cout<<name;
+					line += name;
+				}
+			} catch(...) {
+				cout<<"There was an error !\n";
+			}
+			return line;
+		}
+
+		void yakshaAssert(string testName, int result, string testType) {
+			TestResults testResults;
+			map<std::string, TestCaseResult> testCaseResults;
+			customData = readCustomFileData("../custom.ih");
+			string resultStatus = "Failed";
+			int resultScore = 0;
+			if (result) {
+				resultScore = 1;
+				resultStatus = "Passed";
+			}
+			string testTypeB = "functional";
+			testCaseResults.insert(std::make_pair(GUID, TestCaseResult(testName, testType, 1, resultScore, resultStatus, true, "")));
+			testResults.setTestCaseResults(testCaseResultAsJsonString(GUID, TestCaseResult(testName, testType, 1, resultScore, resultStatus, true, "")));
+			testResults.setCustomData(customData);
+			string finalResult = testResultsAsJsonString(testResults);
+			curlpp::Cleanup cleanup;
+			curlpp::Easy request;
+			request.setOpt(curlpp::options::Url(std::string("https://yaksha-prod-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=jSTWTxtQ8kZgQ5FC0oLgoSgZG7UoU9Asnmxgp6hLLvYId/GW9ccoLw==")));
+			std::list<std::string> header = {
+				"Content-Type: application/json",
+				"accept: application/json"
+			}; 
+			request.setOpt(new curlpp::options::HttpHeader(header));			
+			std::string query = finalResult;
+			request.setOpt(new curlpp::options::PostFields(query)); 
+			request.setOpt(new curlpp::options::WriteStream(&std::cout));
+			request.perform();
+		}
+
+		string testCaseResultAsJsonString(string GUID, TestCaseResult testCaseResult) {
+			json j = {
+				{GUID, {
+					{"methodName", testCaseResult.getMethodName()},
+					{"methodType", testCaseResult.getMethodType()},
+					{"actualScore", testCaseResult.getActualScore()},
+					{"earnedScore", testCaseResult.getEarnedScore()},
+					{"status", testCaseResult.getStatus()},
+					{"isMandatory", testCaseResult.getIsMandatory()},
+					{"errorMessage", testCaseResult.getErroMessage()}
+				}},
+			};
+			return j.dump();
+		}
+
+		string testResultsAsJsonString(TestResults testResults) {
+			json j = {
+				{"testCaseResults", testResults.getTestCaseResults()},
+				{"customData", testResults.getCustomData()}
+			};
+			return j.dump();
+		}
+};
